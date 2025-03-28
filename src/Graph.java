a74d6b2786a09203e196b0ffc522d3a8e2304136
Brian Villanueva <brian.villanueva@student.vinci.be>
2025-03-28 11:46:48 +0100

    commit nul

--- src/Graph.java
+++ src/Graph.java
@@ -6,18 +6,23 @@ import java.util.*;
 
 public class Graph {
     private Map<Integer, Map<Artists, Integer>> nbMentionDArtistesParArtiste;
-    private Map<Integer, Artists> MapArtists ;
+    private Map<Integer, Artists> mapArtists ;
+    private Map<String, Integer> mapArtistParNom;
+    private Map<String, List<Integer>> listDArcs;
 
-    public Graph(File artists, File mentions) {
-        nbMentionDArtistesParArtiste = new HashMap<Integer, Map<Artists, Integer>>();
-        MapArtists = new HashMap<Integer, Artists>();
+    public Graph(String artists, String mentions) {
+        nbMentionDArtistesParArtiste = new HashMap<>();
+        mapArtists = new HashMap<>();
+        listDArcs = new HashMap<>();
+        mapArtistParNom = new HashMap<>();
 
         try (BufferedReader brArtist = new BufferedReader(new FileReader(artists))) {
             String artistLine;
 
             while ((artistLine = brArtist.readLine()) != null) {
                 String[] artist = artistLine.split(",");
-                MapArtists.put(Integer.parseInt((artist[0])), new Artists(Integer.parseInt(artist[0]), artist[1], artist[2]));
+                mapArtistParNom.put(artist[1], Integer.parseInt(artist[0]));
+                mapArtists.put(Integer.parseInt((artist[0])), new Artists(Integer.parseInt(artist[0]), artist[1], artist[2]));
             }
 
         } catch (IOException e) {
@@ -31,11 +36,36 @@ public class Graph {
             while ((mentionLine = brMentions.readLine()) != null) {
                 String[] mention = mentionLine.split(",");
                 Map<Artists, Integer> map = new HashMap<>();
-                map.put(MapArtists.get(Integer.parseInt(mention[1])),Integer.parseInt(mention[2]));
+                map.put(mapArtists.get(Integer.parseInt(mention[1])),1/Integer.parseInt(mention[2]));
                 nbMentionDArtistesParArtiste.put(Integer.parseInt(mention[0]), map);
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    public void trouverCheminLePlusCourt(String artistSource , String artistMentionne) {
+        List file = new ArrayList<Objects>();
+        Boolean trouve = false;
+        int longueurChemin = 0;
+        double coupChemin = 0;
+        int idArtisteSource = mapArtistParNom.get(artistSource);
+        int idArtisteMentionne = mapArtistParNom.get(artistMentionne);
+        while (!trouve){
+            Map<Artists, Integer> listArtistMentionne = nbMentionDArtistesParArtiste.get(idArtisteSource);
+            file.addAll(listArtistMentionne.keySet());
+            if (listArtistMentionne.containsKey(idArtisteMentionne)) {
+                System.out.println( "Longueur du chemin : " + longueurChemin + "\n" +
+                        "Coût total du chemin : " + coupChemin + "\n" +
+                        "Chemin :\n");
+                trouve = true;
+            }
+        }
+
+    }
+
+    public void trouverCheminMaxMentions(String artistSource , String artistMentionne) {
+
+    }
+
 }
--- src/Mentions.java
+++ src/Mentions.java
@@ -1,8 +1,10 @@
 public class Mentions {
+    private String id;
     private int idArtiste;
     private int idArtisteMentionne;
     private int nbMention;
-    public Mentions(int idArtiste, int idArtisteMentionne, int nbMention) {
+    public Mentions(String id, int idArtiste, int idArtisteMentionne, int nbMention) {
+        this.id = id;
         this.idArtiste = idArtiste;
         this.idArtisteMentionne = idArtisteMentionne;
         this.nbMention = nbMention;
